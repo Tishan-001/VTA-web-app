d17410bb452a25ad3f71daa89bc5c0f02e006c0d
Tishan-001 <tishanshamika200@gmail.com>
2024-05-16 23:32:04 +0530

    change 001

--- src/pages/TourGuidersUI/index.jsx
+++ src/pages/TourGuidersUI/index.jsx
@@ -43,23 +43,13 @@ const TourGuidersUIPage = () => {
               src="images/img_rectangle4_1.png"
               alt="rectangleFour"
             />
-
-
             <div className="absolute flex flex-col md:gap-10 gap-[399px] justify-start right-[8%] top-[5%] w-[76%] mt-[450px] md:mt-[100px]">
-             
-             
-             
-                  
-
                   <section>
                     <SearchBar/>
                   </section>
             </div>
           </div>
         </div>
-
-
-
         <div className="flex flex-col items-start justify-start max-w-[1334px] mt-[120px] md:mt-[50px] mx-auto md:px-5 w-full">
           <Text className="text-black-900_b2 text-xl" size="txtInterMedium20">
             Tour Guiders
@@ -70,238 +60,56 @@ const TourGuidersUIPage = () => {
           >
             Our Best Tour Guiders
           </Text>
-         
-         
-         
-          <div className="md:gap-5 gap-[70px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-4 justify-center min-h-[auto] mt-9 w-[105%]  md:w-full md:ml-auto  ">
-           
-         
-           
 
-
-
-            <div className="h-[450px] relative rounded-[20px] shadow-bs w-full">
-              <Img
-                className="h-[450px] m-auto object-cover rounded-[20px] w-full"
-                src="images/img_52_t.png"
-                alt="FiftyTwo"
-              />
-              <div className="absolute flex flex-col md:gap-50 gap-[280px] h-max inset-[0] justify-center m-auto w-[93%]">
-                <div className="bg-gray-700_66_01 flex flex-row gap-2.5 items-center justify-end md:ml-[240px] sm:ml-[280px] ml-[188px] mr-1 p-[5px] rounded-[18px] w-[29%] md:w-[29%]">
-                  <Img
-                    className="h-[21px] w-[22px] "
-                    src="images/img_star6.svg"
-                    alt="starSix"
-                  />
-                  <Text
-                    className="mr-4 text-white-A700_01 text-xl"
-                    size="txtInterMedium20WhiteA70001"
-                  >
-                    4.9
-                  </Text>
-                </div>
-                <div className="bg-gray-600_99 flex flex-col items-center justify-end p-6 rounded-[20px] w-full">
-                  <div className="flex flex-row items-start justify-between mt-[5px] w-[98%] md:w-full">
-                    <div className="flex flex-col items-start justify-start ">
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterSemiBold20"
-                      >
-                        James Petter
-                      </Text>
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterMedium20WhiteA700"
-                      >
-                        $150
-                      </Text>
-                    </div>
-                    <Button
-                     className="absolute hover:bg-[#CAF4FF] hover:text-black-900 cursor-pointer font-bold h-max inset-y-[0] leading-[normal] min-w-[130px] my-auto right-[7%] rounded-[15px] text-center text-1xl mb-[10px]"
-                      shape="round"
+          <div className="md:gap-5 gap-[70px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-4 justify-center min-h-[auto] mt-9 w-[105%] md:w-full md:ml-auto">
+            {tourGuides.map((tourGuide, index) => (
+              <div key={index} className="h-[450px] relative rounded-[20px] shadow-bs w-full">
+                <Img
+                  className="h-[450px] m-auto object-cover rounded-[20px] w-full"
+                  src={tourGuide.media}
+                  alt={tourGuide.name}
+                />
+                <div className="absolute flex flex-col md:gap-50 gap-[280px] h-max inset-[0] justify-center m-auto w-[93%]">
+                  <div className="bg-gray-700_66_01 flex flex-row gap-2.5 items-center justify-end md:ml-[240px] sm:ml-[280px] ml-[188px] mr-1 p-[5px] rounded-[18px] w-[29%] md:w-[29%]">
+                    <Img
+                      className="h-[21px] w-[22px] "
+                      src="images/img_star6.svg"
+                      alt="starSix"
+                    />
+                    <Text
+                      className="mr-4 text-white-A700_01 text-xl"
+                      size="txtInterMedium20WhiteA70001"
                     >
-                      <Link to="/guidedetil">Book Now</Link>
-               
-                    </Button>
+                      {tourGuide.starRating}
+                    </Text>
+                  </div>
+                  <div className="bg-gray-600_99 flex flex-col items-center justify-end p-6 rounded-[20px] w-full">
+                    <div className="flex flex-row items-start justify-between mt-[5px] w-[98%] md:w-full">
+                      <div className="flex flex-col items-start justify-start ">
+                        <Text
+                          className="text-white-A700 text-xl"
+                          size="txtInterSemiBold20"
+                        >
+                          {tourGuide.name}
+                        </Text>
+                        <Text
+                          className="text-white-A700 text-xl"
+                          size="txtInterMedium20WhiteA700"
+                        >
+                          ${tourGuide.price}
+                        </Text>
+                      </div>
+                      <Button
+                        className="absolute hover:bg-[#CAF4FF] hover:text-black-900 cursor-pointer font-bold h-max inset-y-[0] leading-[normal] min-w-[130px] my-auto right-[7%] rounded-[15px] text-center text-1xl mb-[10px]"
+                        shape="round"
+                      >
+                        <Link to={`/guidedetil/${tourGuide.id}`}>Book Now</Link>
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-
-
-
-
-            
-            <div className="h-[450px] relative rounded-[20px] shadow-bs w-full">
-              <Img
-                className="h-[450px] m-auto object-cover rounded-[20px] w-full"
-                src="images/img_52_t.png"
-                alt="FiftyTwo"
-              />
-              <div className="absolute flex flex-col md:gap-50 gap-[280px] h-max inset-[0] justify-center m-auto w-[93%]">
-                <div className="bg-gray-700_66_01 flex flex-row gap-2.5 items-center justify-end md:ml-[0] ml-[188px] mr-1 p-[5px] rounded-[18px] w-[29%] md:w-full">
-                  <Img
-                    className="h-[21px] w-[22px] "
-                    src="images/img_star6.svg"
-                    alt="starSix"
-                  />
-                  <Text
-                    className="mr-4 text-white-A700_01 text-xl"
-                    size="txtInterMedium20WhiteA70001"
-                  >
-                    4.9
-                  </Text>
-                </div>
-                <div className="bg-gray-600_99 flex flex-col items-center justify-end p-6 rounded-[20px] w-full">
-                  <div className="flex flex-row items-start justify-between mt-[5px] w-[98%] md:w-full">
-                    <div className="flex flex-col items-start justify-start ">
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterSemiBold20"
-                      >
-                        James Petter
-                      </Text>
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterMedium20WhiteA700"
-                      >
-                        $150
-                      </Text>
-                    </div>
-                    <Button
-                     className="absolute hover:bg-[#CAF4FF] hover:text-black-900 cursor-pointer font-bold h-max inset-y-[0] leading-[normal] min-w-[130px] my-auto right-[7%] rounded-[15px] text-center text-1xl mb-[10px]"
-                      shape="round"
-                    >
-                      <Link to="/guidedetil">Book Now</Link>
-               
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-            
-            <div className="h-[450px] relative rounded-[20px] shadow-bs w-full">
-              <Img
-                className="h-[450px] m-auto object-cover rounded-[20px] w-full"
-                src="images/img_52_t.png"
-                alt="FiftyTwo"
-              />
-              <div className="absolute flex flex-col md:gap-50 gap-[280px] h-max inset-[0] justify-center m-auto w-[93%]">
-                <div className="bg-gray-700_66_01 flex flex-row gap-2.5 items-center justify-end md:ml-[0] ml-[188px] mr-1 p-[5px] rounded-[18px] w-[29%] md:w-full">
-                  <Img
-                    className="h-[21px] w-[22px] "
-                    src="images/img_star6.svg"
-                    alt="starSix"
-                  />
-                  <Text
-                    className="mr-4 text-white-A700_01 text-xl"
-                    size="txtInterMedium20WhiteA70001"
-                  >
-                    4.9
-                  </Text>
-                </div>
-                <div className="bg-gray-600_99 flex flex-col items-center justify-end p-6 rounded-[20px] w-full">
-                  <div className="flex flex-row items-start justify-between mt-[5px] w-[98%] md:w-full">
-                    <div className="flex flex-col items-start justify-start ">
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterSemiBold20"
-                      >
-                        James Petter
-                      </Text>
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterMedium20WhiteA700"
-                      >
-                        $150
-                      </Text>
-                    </div>
-                    <Button
-                     className="absolute hover:bg-[#CAF4FF] hover:text-black-900 cursor-pointer font-bold h-max inset-y-[0] leading-[normal] min-w-[130px] my-auto right-[7%] rounded-[15px] text-center text-1xl mb-[10px]"
-                      shape="round"
-                    >
-                      <Link to="/guidedetil">Book Now</Link>
-               
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            
-            <div className="h-[450px] relative rounded-[20px] shadow-bs w-full">
-              <Img
-                className="h-[450px] m-auto object-cover rounded-[20px] w-full"
-                src="images/img_52_t.png"
-                alt="FiftyTwo"
-              />
-              <div className="absolute flex flex-col md:gap-50 gap-[280px] h-max inset-[0] justify-center m-auto w-[93%]">
-                <div className="bg-gray-700_66_01 flex flex-row gap-2.5 items-center justify-end md:ml-[0] ml-[188px] mr-1 p-[5px] rounded-[18px] w-[29%] md:w-full">
-                  <Img
-                    className="h-[21px] w-[22px] "
-                    src="images/img_star6.svg"
-                    alt="starSix"
-                  />
-                  <Text
-                    className="mr-4 text-white-A700_01 text-xl"
-                    size="txtInterMedium20WhiteA70001"
-                  >
-                    4.9
-                  </Text>
-                </div>
-                <div className="bg-gray-600_99 flex flex-col items-center justify-end p-6 rounded-[20px] w-full">
-                  <div className="flex flex-row items-start justify-between mt-[5px] w-[98%] md:w-full">
-                    <div className="flex flex-col items-start justify-start ">
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterSemiBold20"
-                      >
-                        James Petter
-                      </Text>
-                      <Text
-                        className="text-white-A700 text-xl"
-                        size="txtInterMedium20WhiteA700"
-                      >
-                        $150
-                      </Text>
-                    </div>
-                    <Button
-                     className="absolute hover:bg-[#CAF4FF] hover:text-black-900 cursor-pointer font-bold h-max inset-y-[0] leading-[normal] min-w-[130px] my-auto right-[7%] rounded-[15px] text-center text-1xl mb-[10px]"
-                      shape="round"
-                    >
-                      <Link to="/guidedetil">Book Now</Link>
-               
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-
-
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            ))}
           </div>
         </div>
         <Footer className="bg-black-900_01 flex items-center justify-center mt-[220px] md:px-5 w-full" />
